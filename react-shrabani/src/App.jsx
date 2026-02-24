a2949af87bdc944e21f71614ab1d4832d73bd77b
Shrabani Mishra <shrabani082002@gmail.com>
2026-02-25 01:05:04 +0530

    Refactored App component using JSX (React.createElement explained)

--- react-shrabani/src/App.jsx
+++ react-shrabani/src/App.jsx
@@ -1,5 +1,11 @@
-export const App=()=>{
-  return(
-    <h1>Welcome to React Shrabani</h1>
-  )
-}
+// import React from "react"
+
+// export const App=()=>{
+//   return(
+//     <h1>Welcome to React Shrabani</h1>
+//   )
+// }
+//when you write jsx babel convert jsx to react.createElement() method and it will create the element and render on the dom
+// export const App=()=>{
+//   return React.createElement("h1",null,"welcome to react shrabani");
+// }
